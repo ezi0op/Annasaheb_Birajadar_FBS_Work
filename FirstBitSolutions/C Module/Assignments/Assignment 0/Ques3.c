2ede177871d9bd672779fbcedd472a0e915f8dba
Annasaheb Ramagonda Birajadar <157781556+ezi0op@users.noreply.github.com>
2025-07-29 15:09:09 +0530

    Add files via upload

---
+++ FirstBitSolutions/C Module/Assignments/Assignment 0/Ques3.c
@@ -0,0 +1,10 @@
+//3. Write a C program to convert temperature from Celsius to Fahrenheit using the
+//formula:
+//F = (C *9/5) + 32
+#include <stdio.h>
+void main(){
+	int c =56;
+	double f;
+	f = (c *1.4) + 32;
+	printf("The temp of Fahrenhet is %lf",f );
+}
