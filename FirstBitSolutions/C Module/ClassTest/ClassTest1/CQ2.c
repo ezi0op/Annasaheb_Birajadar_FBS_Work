a6243d9f5cebd195b6cc3653ad9e9d4fee7bea64
Annasaheb Ramagonda Birajadar <157781556+ezi0op@users.noreply.github.com>
2025-07-30 19:20:34 +0530

    Add files via upload

---
+++ FirstBitSolutions/C Module/ClassTest/ClassTest1/CQ2.c
@@ -0,0 +1,16 @@
+//Que 2. WAP to check if the year is leap or not.
+#include <stdio.h>
+void main()
+{
+	int year;
+	year =  2025;
+	
+	if(year%4==0&&year%4!=0)
+	{
+		printf("It is leaf Year");
+	}
+	else
+	{
+		printf("It is not leaf Year");
+	}
+}
