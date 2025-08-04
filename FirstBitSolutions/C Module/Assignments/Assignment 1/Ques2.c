ef2e424e97e7ee4bd97f75065845b58cdbdbfc36
Annasaheb Ramagonda Birajadar <157781556+ezi0op@users.noreply.github.com>
2025-08-04 22:17:42 +0530

    Ques2.c

--- FirstBitSolutions/C Module/Assignments/Assignment 1/Ques2.c
+++ FirstBitSolutions/C Module/Assignments/Assignment 1/Ques2.c
@@ -1,15 +1,12 @@
 //2. Write a program to check given 3 digit number is pallindrome or not.
 #include <stdio.h>
-void main()
-{
-	int a = 123;
-	
-	if(a%100==a%10)
-	{
-		printf("It is Pallindrome");
-	}
-	else
-	{
-		printf("Its is not Pallindrome");
+void main(){
+	int n=141;
+	int fd=n/100;
+	int ld=n%10;
+	if(fd==ld){
+		printf("%d and %d Pallindrome",fd,ld);
+	}else{
+		printf("%d and %d not Pallindrome",fd,ld);
 	}
 }
